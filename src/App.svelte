d7ccc37f69ab0cdb2fda881609cde49f9e13015e
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-01 01:24:58 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Game from "./components/Game.svelte";
+</script>
+
+<main class="flex justify-center h-screen bg-slate-900 text-yellow-800">
+    <div class="mt-28 text-6xl font-bold">
+        <Game />
+    </div>
+</main>
---
+++ src/components/Game.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from "svelte";
+    let isActive = false;
+    const testPrompt = "ABWKFJ";
+
+    function start() {
+        console.log("Start!");
+        isActive = true;
+    }
+
+    function handleKey(e) {
+        if (!isActive) return;
+        console.log(e.key.toUpperCase());
+    }
+
+    onMount(() => {
+        window.addEventListener("keydown", handleKey);
+        return () => window.removeEventListener("keydown", handleKey);
+    });
+</script>
+
+<div class="flex flex-col mt-5 gap-5 text-center justify-center">
+    <h1>THE GAME</h1>
+    <button onclick={start}>Start</button>
+    <button>Stop</button>
+</div>
