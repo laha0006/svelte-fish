489fea865050c068ce3f0d043db96c01502f84b0
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-01 02:27:08 +0200

    working on typing quick time event, WIP

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import KeyDisplay from "./KeyDisplay.svelte";
     let isActive = false;
     const testPrompt = "ABWKFJ";
 
@@ -8,8 +9,26 @@
         isActive = true;
     }
 
+    function createKeyDisplay(prompt) {
+        let keys = [];
+        for (const char of prompt) {
+            keys.push({
+                key: char,
+                isPressed: false,
+            });
+        }
+        return keys;
+    }
+
+    const keys = $state(createKeyDisplay(testPrompt));
+
     function handleKey(e) {
         if (!isActive) return;
+        for (const key of keys) {
+            if (key.key == e.key.toUpperCase()) {
+                key.isPressed = true;
+            }
+        }
         console.log(e.key.toUpperCase());
     }
 
@@ -21,6 +40,11 @@
 
 <div class="flex flex-col mt-5 gap-5 text-center justify-center">
     <h1>THE GAME</h1>
+    <div class="flex justify-center gap-5">
+        {#each keys as key}
+            <KeyDisplay key={key.key} isPressed={key.isPressed} />
+        {/each}
+    </div>
     <button onclick={start}>Start</button>
     <button>Stop</button>
 </div>
---
+++ src/components/KeyDisplay.svelte
@@ -0,0 +1,9 @@
+<script>
+    const { key, isPressed = false } = $props();
+</script>
+
+{#if isPressed}
+    <p class="text-6xl font-light text-white opacity-30">{key}</p>
+{:else}
+    <p class="text-6xl font-bold text-white">{key}</p>
+{/if}
